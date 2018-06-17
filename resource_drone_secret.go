f717d153edf706da89702801a43e502e2323309d
ael <ael@macremote>
2018-06-17 23:08:27 +0200

    basic secret resource working

--- resource_drone_secret.go
+++ resource_drone_secret.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strings"
+	"fmt"
+	"errors"
 	"github.com/drone/drone-go/drone"
 	"github.com/hashicorp/terraform/helper/schema"
 	"golang.org/x/oauth2"
@@ -26,6 +29,11 @@ func droneSecret() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+                        "events": &schema.Schema{
+                                Type:     schema.TypeList,
+                                Elem:     &schema.Schema{Type: schema.TypeString},
+                                Optional: true,
+                        },
 		},
 	}
 }
@@ -39,26 +47,30 @@ func resourceSecretCreate(d *schema.ResourceData, m interface{}) error {
 		},
 	)
 	repository := d.Get("repository").(string)
+        repositoryNameParts := strings.Split(repository, "/")
+        if len(repositoryNameParts) != 2 {
+                return errors.New("repo name must be 'org/name'")
+        }
+
 	name := d.Get("name").(string)
 	value := d.Get("value").(string)
+	events := d.Get("events").([]string)
+	fmt.Println(events)
 
 	client := drone.NewClient(host, auther)
 
 	secret := drone.Secret{
-		Name: "jlkjlkj",
-		Value : "qsdqdsd",
+		Name: name,
+		Value: value,
+	//	Events: events,
 	}
 
-	_ = secret
-	_ = client
-	_ = repository
-	_ = value
-	_, err := client.SecretCreate(repository, name, nil)
+	_, err := client.SecretCreate(repositoryNameParts[0], repositoryNameParts[1], &secret)
 	if err != nil {
 		return err
 	}
 
-	//d.SetId(name)
+	d.SetId(name)
 
 	return nil
 }
@@ -72,6 +84,29 @@ func resourceSecretUpdate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceSecretDelete(d *schema.ResourceData, m interface{}) error {
+	d.SetId("")
+	config := new(oauth2.Config)
+	auther := config.Client(
+		oauth2.NoContext,
+		&oauth2.Token{
+			AccessToken: token,
+		},
+	)
+	repository := d.Get("repository").(string)
+        repositoryNameParts := strings.Split(repository, "/")
+        if len(repositoryNameParts) != 2 {
+                return errors.New("repo name must be 'org/name'")
+        }
+
+	name := d.Get("name").(string)
+
+	client := drone.NewClient(host, auther)
+
+	_, err := client.Secret(repositoryNameParts[0], repositoryNameParts[1], name)
+	if err != nil {
+		return err
+	}
+
 	d.SetId("")
 	return nil
 }
--- test.tf
+++ test.tf
@@ -1,10 +1,10 @@
 resource "drone_activated_repository" "terraform-provider-drone" {
     name = "ael-g/terraform-provider-drone"
     hooks = [ "push", "pull_request"]
-#    is_trusted = true
+    is_trusted = true
 }
 
-#resource "drone_activated_repository" "est" {
+#resource "drone_activated_repository" "test" {
 #    name = "ael-g/cicd"
 #}
 
@@ -12,5 +12,6 @@ resource "drone_secret" "okook" {
     name = "yeah"
     repository = "ael-g/terraform-provider-drone"
     value = "qsdkhqskdjqhksdjh"
+    events = [ "push", "pull_request"]
 }
 
